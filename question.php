1c2ecd22e6a5d1c32c368a32194c518e0434db4e
Redseba <2004jonathanmorales@gmail.com>
2026-04-16 21:03:37 -0400

    Added Questions and Answers for Board

---
+++ question.php
@@ -0,0 +1,69 @@
+<?php
+// Categories, Questions, and Points
+$categories = [
+    "Capital Cities" => [
+        100 => "What is the Capital City of Georgia?",
+        200 => "What is the Capital City of Texas?",
+        300 => "What is the Capital City of New York?",
+        400 => "What is the Capital City of Hawaii?"
+    ],
+    "Neighboring States" => [
+        100 => "What is the Neighboring state to the left of Georgia?",
+        200 => "What is the Neighboring state to the left of Nevada?",
+        300 => "What is the Neighboring state below New York?",
+        400 => "What is the Neighboring state to the left of Rhode Island?"
+    ],
+    "Landmarks" => [
+        100 => "What is the name of the national park located mostly in Wyoming that was the world's first national park with geysers that constantly erupt?",
+        200 => "What is the national monument in Arizona with a steep, horseshoe-shaped canyon?",
+        300 => "What is the large stone carving of four US presidents in South Dakota?",
+        400 => "What is the name of the famous cracked bell in Philadelphia which is a symbol of American independence?"
+    ],
+    "Celebrity Birthplaces" => [
+        100 => "Which singer was born in Camden, New Jersey, and sang 'Poker Face'?",
+        200 => "Which actor was born in London, Kentucky, and starred in The Hunger Games?",
+        300 => "Which actor was born in Los Angeles, California, and played Iron Man in the Marvel movies?",
+        400 => "Which boxer was born in Louisville, Kentucky, and was known as 'The Greatest'?"
+    ],
+    "Universities" => [
+        100 => "Which public university in located in Atlanta, Georgia, has the Panther as its mascot?",
+        200 => "Which university in Berkeley, California, is the flagship of the UC system and has a Golden Bear mascot?",
+        300 => "Which private Ivy League university is located in New Haven, Connecticut?",
+        400 => "Which university is, located in South Bend, Indiana, is known for its Fighting Irish football team?"
+    ]
+];
+
+// Answers
+$answers = [
+    "Capital Cities" => [
+        100 => "Atlanta",
+        200 => "Austin",
+        300 => "Albany",
+        400 => "Honolulu"
+    ],
+    "Neighboring States" => [
+        100 => "Alabama",
+        200 => "California",
+        300 => "Pennsylvania",
+        400 => "Connecticut"
+    ],
+    "Landmarks" => [
+        100 => "Yellowstone",
+        200 => "Grand Canyon",
+        300 => "Mount Rushmore",
+        400 => "Liberty Bell"
+    ],
+    "Celebrity Birthplaces" => [
+        100 => "Lady Gaga",
+        200 => "Jennifer Lawrence",
+        300 => "Robert Downey Jr",
+        400 => "Muhammed Ali"
+    ],
+    "Universities" => [
+        100 => "Georgia State University",
+        200 => "University of California",
+        300 => "Yale University",
+        400 => "University of Notre Dame"
+    ]
+];
+?>
